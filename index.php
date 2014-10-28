ce591726bed604f170619ff0435e125c85180ad7
Jordan Abujaber <jajaber13@gmail.com>
2014-10-28 08:53:13 -0700

    hello 4

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php 
   require_once(__DIR__ . "/controller/create-db.php");
+  // The  DIR basically takes you back to the directory, and directs you to the controller file.
   ?>
--- model/database.php
+++ model/database.php
@@ -3,3 +3,4 @@
     $username = "root";
     $password = "root";
     $database = "blog_db";
+// these are the varables
