3f6cf3c00f6c5b3096f6ab60a8b613b4353c3812
Jordan Abujaber <jajaber13@gmail.com>
2014-10-30 08:29:52 -0700

    hello 6

--- controller/create-db.php
+++ controller/create-db.php
@@ -36,5 +36,22 @@
      	echo "Database has already been created.";
      	// lets you know that data base has been created and exists
      }
+    
+     $query = $connection->query("CREATE TABLE posts ("
+      . "id int(11) NOT NULL AURO_INCREMENT,"
+      . "title varchar(255) NOT NULL,"
+      . "post text NOT NULL"
+      . "PRIMARY KEY  (id))");
+     // we are running a query on this database.
+     // we are creating a table for posts.
+     // this is a table that can store all the blog posts and save it on the data base.
+     // we gived the id's numerical values
+     // we have over a billion id's that we can generate
+    // NOT NULL indicates that the blog post can not be empty
+    // Auto increment it is going to increment the id numbers and it will handle the id's for us. The actual database handles it for us.    
+    // we made a post column, and it can not be empty
+  // since we are adding more than one id we need , 
+  // Primary Key will let us select info from these posts, it is the way tables are connected to each other.
+  // everything has to be set prior to being put in the table.
    $connection->close();
    // End of your connection variable
