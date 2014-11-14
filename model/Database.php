5555bb6d8d9775bf4f72ed82554d6e36400b3b04
Jordan Abujaber <jajaber13@gmail.com>
2014-11-14 08:53:26 -0800

    Video 26 Creating a Database Class

---
+++ model/Database.php
@@ -0,0 +1,13 @@
+<?php
+class Database {
+// when we set this to private it means we can only access this variable in this file
+	private $connection;
+	private $host;
+	private $username;
+	private $password;
+	private $database
+// we have created instance variables and global variables, we have also set the visibility
+// we wont be able to access these variables in any other file, only in these classes
+// this way when we create a new object, these variables are hidden and nobody else can access them and modify them
+// adding these classes makes the code easier to maintiain and easier to read
+}
