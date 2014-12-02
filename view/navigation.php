dde6b81f8797fb2eba296f91aa697aad45b88f7d
Jordan Abujaber <jajaber13@gmail.com>
2014-12-01 20:39:56 -0800

    my test for the animation

--- view/navigation.php
+++ view/navigation.php
@@ -7,10 +7,10 @@
 
      <ul>
         <li><a href="<?php echo $path . "post.php" ?>"> Blog post form</a> </li>
-       <!-- this is all we should have in our navigation.php file -->
-       <!-- we only have one link and this link is the post.php file -->
+     <!-- this is all we should have in our navigation.php file -->
+     <!-- we only have one link and this link is the post.php file -->
      <!--  right now we are telling it to look at the root server folder aka the local host-->
-    <!--  we are going to tell it the actual location of our post.php file -->
+     <!--  we are going to tell it the actual location of our post.php file -->
      <!--  there are no errors and everything is working correctly -->
      </ul>
 
