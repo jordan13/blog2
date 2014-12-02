f8238d9260299b85efd102ebb2d05ed44f2f3136
Jordan Abujaber <jajaber13@gmail.com>
2014-12-02 08:41:13 -0800

    Video 37 Database table to store registered users

---
+++ view/register-form.php
@@ -0,0 +1,21 @@
+  <form>
+      <div>
+         <label for="email">Email:</label>
+         <input type="text" name="email"/>
+      </div>
+      
+      <div>
+         <label for="username">Username: </label>
+         <input type="text" name="username" />
+      </div>
+      
+      <div>
+        <label for="password">Password: </label>
+        <input type="password" name="password" />
+        <!-- the reason why the type is not text is to hide the password while typing the password-->
+      </div>
+
+      <div> 
+        <button type="submit">Submit</button>
+      </div>
+  </form>
