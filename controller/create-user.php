046a52173fdb5a819c90cb8dc3b317af25a9ddea
Jordan Abujaber <jajaber13@gmail.com>
2014-12-05 08:52:54 -0800

    Video 39 Attaching a Controller to the Register Form

---
+++ controller/create-user.php
@@ -0,0 +1,3 @@
+<?php 
+	echo "Succesfully created user";
+	// this is just showing that we succesfully displayed the user
--- view/register-form.php
+++ view/register-form.php
@@ -1,4 +1,11 @@
-  <form>
+ <?php 
+     require_once(__DIR__ . "/../model/config.php");
+     // this allows us to access other files form the directory
+ ?>
+<!-- This indicates that this form is to register -->
+<h1> Register</h1>
+
+  <form method="post" action="<?php echo $path . "controller/create-user.php"; ?>">
       <div>
          <label for="email">Email:</label>
          <input type="text" name="email"/>
