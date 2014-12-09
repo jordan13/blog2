1817eaaa9afa1d0b7996d43cc0470cf191bc4213
Jordan Abujaber <jajaber13@gmail.com>
2014-12-09 08:55:44 -0800

    Video 43 Creating a Salt for our Password

--- controller/create-user.php
+++ controller/create-user.php
@@ -6,4 +6,12 @@
     $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
     $passworod = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
-    echo $email . " = " . $username . " = " . $password;
+    echo $password;
+
+    $salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
+
+    echo $salt;
+    // we are telling it to use huge random numbers to create unique id's
+    // uniqid creates unique ids for us and makes it random 
+    // the minimum we should use is 5000 rounds
+    // we are going to split this up because php reads the $'s as variables'
