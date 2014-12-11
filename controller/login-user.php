fe41c9dcd0c896186567e4ead1c2639bdeaa3c54
Jordan Abujaber <jajaber13@gmail.com>
2014-12-11 07:58:05 -0800

    Video 47  Selecting User Info from the data base

--- controller/login-user.php
+++ controller/login-user.php
@@ -1,2 +1,9 @@
 <?php
-	echo "Succesfully logged in user";
+	require_once(__DIR__ . "/../model/config.php");
+	// this gives us access to our data base
+	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+	// we put input post because our method is from post
+	$password = filter_input (INPUT_POST, "username", FILTER_SANITIZE_STRING);
+
+	$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
+	// we are selecting our salt and our password from our users table where our username is the username that was sent in via the post
